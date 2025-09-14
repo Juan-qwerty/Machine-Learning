e5cab786b23a4b71fc8c5799a89349f1e42a2e92
Juan-qwerty <Juahddh@gmail.com>
2025-09-14 15:11:58 -0500

    Update: Repository link

--- Clasificador IRIS/ClasificadorIRIS2.py
+++ Clasificador IRIS/ClasificadorIRIS2.py
@@ -14,7 +14,7 @@ iris = load_iris()
 X = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target)
 
-# División en entrenamiento (80%) y prueba (20%)
+# División en entrenamiento (80%) y de prueba (20%)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
